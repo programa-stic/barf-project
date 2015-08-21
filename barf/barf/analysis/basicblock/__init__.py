0b214fd83e2583b1293dcb610655e92b061c47dd
Christian Heitman <cnheitman@fundacionsadosky.org.ar>
2015-08-21 12:20:20 -0300

    Improve CFG recovery functionality.

--- barf/barf/analysis/basicblock/__init__.py
+++ barf/barf/analysis/basicblock/__init__.py
@@ -23,7 +23,6 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 from basicblock import BARF_DISASM_LINEAR
-from basicblock import BARF_DISASM_MIXED
 from basicblock import BARF_DISASM_RECURSIVE
 from basicblock import BasicBlock
 from basicblock import BasicBlockBuilder
