c6d35f241738d8c1b26b7839b1f2ca864da42281
Christian Heitman <cnheitman@fundacionsadosky.org.ar>
2015-09-18 14:16:57 -0300

    Add support for local installation to the install script.

--- barf/install.sh
+++ barf/install.sh
@@ -1,9 +1,24 @@
 #! /bin/bash
 
-# Install solvers
+# Set installation mode
 # ---------------------------------------------------------------------------- #
-./install-solvers.sh
+if [ "$#" -eq 1 ] && [ "$1" == "local" ];
+then
+    echo "[+] Local installation..."
+    # Install solvers
+    # ------------------------------------------------------------------------ #
+    ./install-solvers.sh local
 
-# Install BARF
-# ---------------------------------------------------------------------------- #
-sudo python setup.py
+    # Install BARF
+    # ------------------------------------------------------------------------ #
+    sudo python setup.py install --user
+else
+    echo "[+] System installation..."
+    # Install solvers
+    # ------------------------------------------------------------------------ #
+    ./install-solvers.sh
+
+    # Install BARF
+    # ------------------------------------------------------------------------ #
+    sudo python setup.py install
+fi
