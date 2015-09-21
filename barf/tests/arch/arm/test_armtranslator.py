7a89d7dd783c58397216fb7944ee8368f9279005
Christian Heitman <cnheitman@fundacionsadosky.org.ar>
2015-09-21 16:23:59 -0300

    Refactor (and update) arm translator tests.

--- barf/tests/arch/arm/test_armtranslator.py
+++ barf/tests/arch/arm/test_armtranslator.py
@@ -35,19 +35,19 @@ from barf.arch.arm.armbase import ArmArchitectureInformation
 from barf.arch.arm.armparser import ArmParser
 from barf.arch.arm.armtranslator import ArmTranslator
 from barf.arch.arm.armtranslator import FULL_TRANSLATION
+from barf.core.reil import ReilContainer
 from barf.core.reil import ReilEmulator
+from barf.core.reil import ReilSequence
 
 
-@unittest.skipUnless(platform.machine().lower() == 'armv7l',
-                     'Not running on an ARMv7 system')
+@unittest.skipUnless(platform.machine().lower() == 'armv6l',
+                     'Not running on an ARMv6 system')
 class ArmTranslationTests(unittest.TestCase):
 
     def setUp(self):
         self.trans_mode = FULL_TRANSLATION
         self.arch_mode = ARCH_ARM_MODE_32
-
         self.arch_info = ArmArchitectureInformation(self.arch_mode)
-
         self.arm_parser = ArmParser(self.arch_mode)
         self.arm_translator = ArmTranslator(self.arch_mode, self.trans_mode)
         self.reil_emulator = ReilEmulator(self.arch_info)
@@ -69,7 +69,8 @@ class ArmTranslationTests(unittest.TestCase):
                 min_value, max_value = 0, 2**self.arch_info.operand_size - 1
                 context[reg] = random.randint(min_value, max_value)
 
-        # Only highest 4 bits (N, C, Z, V) are randomized, the rest are left in the default (user mode) value
+        # Only highest 4 bits (N, C, Z, V) are randomized, the rest are
+        # left in the default (user mode) value.
         context['apsr'] = 0x00000010
         context['apsr'] |= random.randint(0x0, 0xF) << 28
 
@@ -157,33 +158,9 @@ class ArmTranslationTests(unittest.TestCase):
 
         for bit, flag in flags.items():
             flag_str = flag.upper() if flags_reg & 2**bit else flag.lower()
-            out +=  flag_str + " "
+            out += flag_str + " "
 
         return out[:-1]
-
-    def __fix_reil_flag(self, reil_context, arm_context, flag):
-        reil_context_out = dict(reil_context)
-
-        flags_reg = 'eflags' if 'eflags' in reil_context_out else 'rflags'
-
-        arch_size = self.arch_info.architecture_size
-
-        _, bit = self.arch_info.alias_mapper[flag]
-
-        # Clean flag.
-        reil_context_out[flags_reg] &= ~(2**bit) & (2**32-1)
-
-        # Copy flag.
-        reil_context_out[flags_reg] |= (arm_context[flags_reg] & 2**bit)
-
-        return reil_context_out
-
-    def __fix_reil_flags(self, reil_context, arm_context):
-        # Remove this when AF and PF are implemented.
-        reil_context_out = self.__fix_reil_flag(reil_context, arm_context, "af")
-        reil_context_out = self.__fix_reil_flag(reil_context, arm_context, "pf")
-
-        return reil_context_out
 
     def __set_address(self, address, arm_instrs):
         addr = address
@@ -193,23 +170,52 @@ class ArmTranslationTests(unittest.TestCase):
             arm_instr.size = 4
             addr += 4
 
-    def _test_asm_instruction(self, asm):
-        print(asm)
+    def __translate(self, asm_instrs):
+        instr_container = ReilContainer()
 
-        arm_instrs = map(self.arm_parser.parse, asm)
+        asm_instr_last = None
+        instr_seq_prev = None
 
-        self.__set_address(0xdeadbeef, arm_instrs)
+        for asm_instr in asm_instrs:
+            instr_seq = ReilSequence()
 
-        reil_instrs = map(self.arm_translator.translate, arm_instrs)
+            for reil_instr in self.arm_translator.translate(asm_instr):
+                instr_seq.append(reil_instr)
 
+            if instr_seq_prev:
+                instr_seq_prev.next_sequence_address = instr_seq.address
+
+            instr_container.add(instr_seq)
+
+            instr_seq_prev = instr_seq
+
+        if instr_seq_prev:
+            if asm_instr_last:
+                instr_seq_prev.next_sequence_address = (asm_instr_last.address + asm_instr_last.size) << 8
+
+        return instr_container
+
+    def __asm_to_reil(self, asm_list, address):
+        arm_instrs = [self.arm_parser.parse(asm) for asm in asm_list]
+
+        self.__set_address(address, arm_instrs)
+
+        reil_instrs = self.__translate(arm_instrs)
+
+        return reil_instrs
+
+    def __run_code(self, asm_list, address, ctx_init):
+        reil_instrs = self.__asm_to_reil(asm_list, address)
+
+        _, arm_ctx_out, _ = pyasmjit.arm_execute("\n".join(asm_list), ctx_init)
+        reil_ctx_out, _ = self.reil_emulator.execute(reil_instrs, start=address << 8, registers=ctx_init)
+
+        return arm_ctx_out, reil_ctx_out
+
+    def __test_asm_instruction(self, asm_list):
         ctx_init = self.__init_context()
 
-        arm_rv, arm_ctx_out, _ = pyasmjit.arm_execute("\n".join(asm), ctx_init)
-        reil_ctx_out, reil_mem_out = self.reil_emulator.execute(
-            reil_instrs,
-            0xdeadbeef << 8,
-            context=ctx_init
-        )
+        arm_ctx_out, reil_ctx_out = self.__run_code(asm_list, 0xdeadbeef, ctx_init)
 
         cmp_result = self.__compare_contexts(ctx_init, arm_ctx_out, reil_ctx_out)
 
@@ -218,132 +224,58 @@ class ArmTranslationTests(unittest.TestCase):
 
         self.assertTrue(cmp_result, self.__print_contexts(ctx_init, arm_ctx_out, reil_ctx_out))
 
-    def _execute_asm(self, asm, ini_addr = 0x8000):
-        print(asm)
+    def __test_asm_instruction_with_mem(self, asm_list, address_register):
+        # TODO: Merge with previous test function.
 
-        arm_instrs = map(self.arm_parser.parse, asm)
+        mem_addr = pyasmjit.arm_alloc(4096)
 
-        self.__set_address(ini_addr, arm_instrs)
+        self.reil_emulator.reset()
 
-        reil_instrs = map(self.arm_translator.translate, arm_instrs)
+        reil_instrs = self.__asm_to_reil(asm_list, 0xdeadbeef)
+
+        ctx_init = self.__init_context()
+        ctx_init[address_register] = mem_addr
+
+        _, arm_ctx_out, arm_mem_out = pyasmjit.arm_execute("\n".join(asm_list), ctx_init)
+        reil_ctx_out, reil_mem_out = self.reil_emulator.execute(reil_instrs, 0xdeadbeef << 8, registers=ctx_init)
+
+        base_addr = mem_addr
+
+        for idx, b in enumerate(struct.unpack("B" * len(arm_mem_out), arm_mem_out)):
+            addr = base_addr + idx
+
+            # TODO: Don't access variable directly.
+            if addr in reil_mem_out._memory:
+                self.assertTrue(b == reil_mem_out.read(addr, 1))
+            else:
+                # Memory in pyasmjit is initialized to 0.
+                self.assertTrue(b == 0x0)
+
+        cmp_result = self.__compare_contexts(ctx_init, arm_ctx_out, reil_ctx_out)
+
+        if not cmp_result:
+            self.__save_failing_context(ctx_init)
+
+        self.assertTrue(cmp_result, self.__print_contexts(ctx_init, arm_ctx_out, reil_ctx_out))
+
+        # NOTE: There is only one memory pool, so there is no need
+        # (for now) to specify the address.
+        pyasmjit.arm_free()
+
+    def __execute_asm(self, asm_list, address=0x8000):
+        reil_instrs = self.__asm_to_reil(asm_list, address)
 
         ctx_init = self.__init_context()
 
-        reil_ctx_out, reil_mem_out = self.reil_emulator.execute(
-            reil_instrs,
-            0xdeadbeef << 8,
-            context=ctx_init
-        )
+        reil_ctx_out, _ = self.reil_emulator.execute(reil_instrs, address << 8, registers=ctx_init)
 
         return reil_ctx_out
 
-    # R11 is used as a dirty register to check if the branch was taken or not
-    def test_asm_branch_instruction(self):
-        untouched_value = 0x45454545
-        touched_value = 0x31313131
+    def test_data_instructions(self):
+        # TODO: R13 (SP), R14 (LR), R15 (PC) are outside testing scope
+        # for now.
 
-        inst_samples_touched = [
-            ["mov r11, #0x{:x}".format(untouched_value),
-               "b #0x800c",
-               "mov r11, #0x{:x}".format(touched_value),
-               "mov r0, r0",
-            ],
-
-            ["mov r11, #0x{:x}".format(untouched_value),
-               "bx #0x800c",
-               "mov r11, #0x{:x}".format(touched_value),
-               "mov r0, r0",
-            ],
-
-            ["mov r11, #0x{:x}".format(untouched_value),
-               "bl #0x800c",
-               "mov r11, #0x{:x}".format(touched_value),
-               "mov r0, r0",
-            ],
-
-            ["mov r11, #0x{:x}".format(untouched_value),
-               "blx #0x800c",
-               "mov r11, #0x{:x}".format(touched_value),
-               "mov r0, r0",
-            ],
-
-            ["movs r11, #0x{:x}".format(untouched_value),
-               "bne #0x800c",
-               "mov r11, #0x{:x}".format(touched_value),
-               "mov r0, r0",
-            ],
-
-            ["mov r11, #0x{:x}".format(untouched_value),
-               "mov r1, #0x8010",
-               "bx r1",
-               "mov r11, #0x{:x}".format(touched_value),
-               "mov r0, r0",
-            ],
-
-            ["mov r11, #0x{:x}".format(untouched_value),
-               "mov r1, #0x8010",
-               "blx r1",
-               "mov r11, #0x{:x}".format(touched_value),
-               "mov r0, r0",
-            ],
-
-        ]
-
-
-        for asm in inst_samples_touched:
-            reil_ctx_out = self._execute_asm(asm, 0x8000)
-            self.assertTrue(reil_ctx_out['r11'] == untouched_value)
-#             print reil_ctx_out
-
-    # TODO: Merge with previous test function
-    def _test_asm_instruction_with_mem(self, asm, reg_mem):
-        print(asm)
-
-        mem_dir = pyasmjit.arm_alloc(4096)
-
-        arm_instrs = map(self.arm_parser.parse, asm)
-
-        self.__set_address(0xdeadbeef, arm_instrs)
-
-        reil_instrs = map(self.arm_translator.translate, arm_instrs)
-
-        ctx_init = self.__init_context()
-
-        ctx_init[reg_mem] = mem_dir
-
-        arm_rv, arm_ctx_out, arm_mem_out = pyasmjit.arm_execute("\n".join(asm), ctx_init)
-
-        self.reil_emulator._mem._memory = {} # TODO: Check how to clean emulator memory.
-
-        reil_ctx_out, reil_mem_out = self.reil_emulator.execute(
-            reil_instrs,
-            0xdeadbeef << 8,
-            context=ctx_init
-        )
-
-        base_dir = mem_dir
-
-        for idx, b in enumerate(struct.unpack("B" * len(arm_mem_out), arm_mem_out)):
-            if (base_dir + idx) in reil_mem_out._memory: # TODO: Don't access variable directly.
-                self.assertTrue(b == reil_mem_out._memory[base_dir + idx])
-            else:
-                self.assertTrue(b == 0x0) # Memory in pyasmjit is initialized to 0
-
-
-        cmp_result = self.__compare_contexts(ctx_init, arm_ctx_out, reil_ctx_out)
-
-        if not cmp_result:
-            self.__save_failing_context(ctx_init)
-
-        self.assertTrue(cmp_result, self.__print_contexts(ctx_init, arm_ctx_out, reil_ctx_out))
-
-        pyasmjit.arm_free() # There is only one memory pool, so there is no need (for now) to specify the address
-
-
-    # TODO: R13 (SP), R14 (LR), R15 (PC) are outside testing scope for now
-
-    def test_data_proc_inst(self):
-        inst_samples = [
+        instr_samples = [
             # No flags
             ["mov r0, r1"],
             ["mov r3, r8"],
@@ -360,22 +292,33 @@ class ArmTranslationTests(unittest.TestCase):
             ["sub r0, r3, r12"],
             ["cmp r3, r12"],
             ["cmn r3, r12"],
-            ['mov r8, r5, lsl r6'],
-            ['eor r8, r4, r5, lsl r6'],
-            ['mul r3, r4, r8'],
-            ["mov r8, #0", 'mul r3, r4, r8'],
-            ['mul r3, r4, r4'],
+            ["mov r8, r5, lsl r6"],
+            ["eor r8, r4, r5, lsl r6"],
+            ["mul r3, r4, r8"],
+            ["mov r8, #0",
+             "mul r3, r4, r8"],
+            ["mul r3, r4, r4"],
 
             # Flags update
             ["movs r0, #0"],
             ["movs r0, #-10"],
-            ["mov r0, #0x7FFFFFFF", "mov r1, r0", "adds r3, r0, r1"],
-            ["mov r0, #0x7FFFFFFF", "mov r1, r0", "subs r3, r0, r1"],
-            ["mov r0, #0x00FFFFFF", "add r1, r0, #10", "subs r3, r0, r1"],
-            ["mov r0, #0xFFFFFFFF", "adds r3, r0, #10"],
-            ["mov r0, #0x7FFFFFFF", "mov r1,  #5", "adds r3, r0, r1"],
-            ["mov r0, #0x80000000", "mov r1,  #5", "subs r3, r0, r1"],
-
+            ["mov r0, #0x7FFFFFFF",
+             "mov r1, r0",
+             "adds r3, r0, r1"],
+            ["mov r0, #0x7FFFFFFF",
+             "mov r1, r0",
+             "subs r3, r0, r1"],
+            ["mov r0, #0x00FFFFFF",
+             "add r1, r0, #10",
+             "subs r3, r0, r1"],
+            ["mov r0, #0xFFFFFFFF",
+             "adds r3, r0, #10"],
+            ["mov r0, #0x7FFFFFFF",
+             "mov r1,  #5",
+             "adds r3, r0, r1"],
+            ["mov r0, #0x80000000",
+             "mov r1,  #5",
+             "subs r3, r0, r1"],
 
             # Flags evaluation
             ["moveq r0, r1"],
@@ -397,29 +340,116 @@ class ArmTranslationTests(unittest.TestCase):
             ["sublo r0, r3, r12"],
         ]
 
+        for instr in instr_samples:
+            self.__test_asm_instruction(instr)
 
-        for i in inst_samples:
-            self._test_asm_instruction(i)
+    def test_mememory_instructions(self):
+        # R12 is loaded with the memory address
 
-    # R12 is loaded with the memory address
-    def test_mem_inst(self):
-        inst_samples = [
-            ["str r0, [r12]", "ldr  r1, [r12]"],
-            ["stm r12!, {r0 - r4}",   "ldmdb r12, {r5 - r9}"],
-            ["stmia r12, {r8 - r9}",  "ldmia r12, {r1 - r2}"],
-            ["stmib r12!, {r11}",    "ldmda r12!, {r3}"],
-            ["add r12, r12, #0x100", "stmda r12, {r9 - r11}",   "ldmda r12, {r1 - r3}"],
-            ["add r12, r12, #0x100", "stmdb r12!, {r3}",   "ldmia r12!, {r9}"],
-            ["add r12, r12, #0x100", "stmfd r12, {r2 - r4}"],
+        instr_samples = [
+            ["str r0, [r12]",
+             "ldr  r1, [r12]"],
+            ["stm r12!, {r0 - r4}",
+             "ldmdb r12, {r5 - r9}"],
+            ["stmia r12, {r8 - r9}",
+             "ldmia r12, {r1 - r2}"],
+            ["stmib r12!, {r11}",
+             "ldmda r12!, {r3}"],
+            ["add r12, r12, #0x100",
+             "stmda r12, {r9 - r11}",
+             "ldmda r12, {r1 - r3}"],
+            ["add r12, r12, #0x100",
+             "stmdb r12!, {r3}",
+             "ldmia r12!, {r9}"],
+            ["add r12, r12, #0x100",
+             "stmfd r12, {r2 - r4}"],
             ["stmfa r12!, {r1 - r4}"],
-            ["add r12, r12, #0x100", "stmed r12, {r6 - r9}"],
+            ["add r12, r12, #0x100",
+             "stmed r12, {r6 - r9}"],
             ["stmea r12!, {r5 - r7}"],
-            ["mov r0, r13", "mov r13, r12", "push {r1 - r10}", "pop {r2 - r11}", "mov r13, r0", "mov r0, #0"], # The last inst. is needed because the emulator has no access to the real value of native r13 (SP) which is not passed in the context
-            ["mov r0, r13", "mov r13, r12", "push {r2 - r11}", "pop {r1 - r10}", "mov r13, r0", "mov r0, #0"],
+            # NOTE: The last instr. is needed because the emulator has
+            # no access to the real value of native r13 (SP) which is
+            # not passed in the context.
+            ["mov r0, r13",
+             "mov r13, r12",
+             "push {r1 - r10}",
+             "pop {r2 - r11}",
+             "mov r13, r0",
+             "mov r0, #0"],
+            ["mov r0, r13",
+             "mov r13, r12",
+             "push {r2 - r11}",
+             "pop {r1 - r10}",
+             "mov r13, r0",
+             "mov r0, #0"],
         ]
 
-        for i in inst_samples:
-            self._test_asm_instruction_with_mem(i, 'r12')
+        for instr in instr_samples:
+            self.__test_asm_instruction_with_mem(instr, 'r12')
+
+    # def test_branch_instructions(self):
+    #     untouched_value = 0x45454545
+    #     touched_value = 0x31313131
+
+    #     # R11 is used as a dirty register to check if the branch was
+    #     # taken or not.
+    #     instr_samples = [
+    #         [
+    #             "mov r11, #0x{:x}".format(untouched_value),
+    #             "b #0x800c",
+    #             "mov r11, #0x{:x}".format(touched_value),
+    #             "mov r0, r0",
+    #         ],
+
+    #         [
+    #             "mov r11, #0x{:x}".format(untouched_value),
+    #             "bx #0x800c",
+    #             "mov r11, #0x{:x}".format(touched_value),
+    #             "mov r0, r0",
+    #         ],
+
+    #         [
+    #             "mov r11, #0x{:x}".format(untouched_value),
+    #             "bl #0x800c",
+    #             "mov r11, #0x{:x}".format(touched_value),
+    #             "mov r0, r0",
+    #         ],
+
+    #         [
+    #             "mov r11, #0x{:x}".format(untouched_value),
+    #             "blx #0x800c",
+    #             "mov r11, #0x{:x}".format(touched_value),
+    #             "mov r0, r0",
+    #         ],
+
+    #         [
+    #             "movs r11, #0x{:x}".format(untouched_value),
+    #             "bne #0x800c",
+    #             "mov r11, #0x{:x}".format(touched_value),
+    #             "mov r0, r0",
+    #         ],
+
+    #         [
+    #             "mov r11, #0x{:x}".format(untouched_value),
+    #             "mov r1, #0x8010",
+    #             "bx r1",
+    #             "mov r11, #0x{:x}".format(touched_value),
+    #             "mov r0, r0",
+    #         ],
+
+    #         [
+    #             "mov r11, #0x{:x}".format(untouched_value),
+    #             "mov r1, #0x8010",
+    #             "blx r1",
+    #             "mov r11, #0x{:x}".format(touched_value),
+    #             "mov r0, r0",
+    #         ],
+    #     ]
+
+    #     for instr in instr_samples:
+    #         reil_ctx_out = self.__execute_asm(instr, 0x8000)
+
+    #         self.assertTrue(reil_ctx_out['r11'] == untouched_value)
 
 
 def main():
