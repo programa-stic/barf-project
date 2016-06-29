d320de7a66472036f2504f8b935747ff4a1e4e49
Christian Heitman <cnheitman@fundacionsadosky.org.ar>
2016-06-29 14:10:26 -0300

    Fix Capstone installation error on virtualenvs

--- barf/setup.py
+++ barf/setup.py
@@ -3,6 +3,28 @@
 from setuptools import setup
 from setuptools import find_packages
 
+
+# https://github.com/aquynh/capstone/issues/583
+def fix_setuptools():
+    """Work around bugs in setuptools.
+
+    Some versions of setuptools are broken and raise SandboxViolation for normal
+    operations in a virtualenv. We therefore disable the sandbox to avoid these
+    issues.
+    """
+    try:
+        from setuptools.sandbox import DirectorySandbox
+
+        def violation(operation, *args, **_):
+            print "SandboxViolation: %s" % (args,)
+
+        DirectorySandbox._violation = violation
+    except ImportError:
+        pass
+
+# Fix bugs in setuptools.
+fix_setuptools()
+
 setup(
     author           = 'Christian Heitman',
     author_email     = 'cnheitman@fundacionsadosky.org.ar',
