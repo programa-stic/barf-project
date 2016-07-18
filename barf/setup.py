1b13e87bee2bab0c092149db8fe88391adff2d3e
Christian Heitman <cnheitman@fundacionsadosky.org.ar>
2016-07-18 12:35:34 -0300

    Prepare package for pypi

--- barf/setup.py
+++ barf/setup.py
@@ -3,6 +3,7 @@
 from setuptools import setup
 from setuptools import find_packages
 
+__version__ = '0.2'
 
 # https://github.com/aquynh/capstone/issues/583
 def fix_setuptools():
@@ -29,6 +30,7 @@ setup(
     author           = 'Christian Heitman',
     author_email     = 'cnheitman@fundacionsadosky.org.ar',
     description      = 'A multiplatform open source Binary Analysis and Reverse engineering Framework',
+    download_url     = 'https://github.com/programa-stic/barf-project/tarball/v0.2',
     install_requires = [
         'capstone',
         'networkx',
@@ -42,13 +44,24 @@ setup(
     ],
     license          = 'BSD 2-Clause',
     name             = 'barf',
-    packages         = find_packages(),
+    classifiers      = [
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Security',
+        'Topic :: Software Development :: Disassemblers',
+        'Topic :: Software Development :: Interpreters',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    packages         = find_packages(exclude=['tests', 'tests.*']),
     url              = 'http://github.com/programa-stic/barf-project',
     scripts          = [
         'tools/cfg/BARFcfg',
         'tools/cg/BARFcg',
         'tools/gadgets/BARFgadgets',
     ],
-    version          = '0.2',
+    version          = __version__,
     zip_safe         = False
 )
