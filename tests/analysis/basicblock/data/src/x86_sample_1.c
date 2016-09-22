ad275bf9bced8205fbcafe4ddf0d27c62a7f230f
Christian Heitman <cnheitman@fundacionsadosky.org.ar>
2016-09-22 16:04:22 -0300

    Add test for the basic block module

---
+++ tests/analysis/basicblock/data/src/x86_sample_1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ * Basic sample, just one function.
+ *
+ */
+
+int main(int argc, char *argv[]) {
+    printf("Hello, World!\n");
+
+    return 0;
+}
--- tests/analysis/basicblock/test_basicblock.py
+++ tests/analysis/basicblock/test_basicblock.py
@@ -22,12 +22,25 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+import os
 import unittest
 
+from barf.analysis.basicblock import CFGRecoverer
+from barf.analysis.basicblock import ControlFlowGraph
+from barf.analysis.basicblock import RecursiveDescent
 from barf.analysis.basicblock.basicblock import BasicBlock
+from barf.arch import ARCH_X86_MODE_32
+from barf.arch.x86.x86base import X86ArchitectureInformation
+from barf.arch.x86.x86disassembler import X86Disassembler
 from barf.arch.x86.x86parser import X86Parser
 from barf.arch.x86.x86translator import X86Translator
+from barf.core.bi import BinaryFile
 from barf.core.reil import DualInstruction
+
+
+def get_full_path(filename):
+    return os.path.dirname(os.path.abspath(__file__)) + filename
+
 
 
 class BinDiffTests(unittest.TestCase):
@@ -82,6 +95,85 @@ class BinDiffTests(unittest.TestCase):
 
         # It will not assert true. Read comment on BasicBlock.__eq__
         # self.assertTrue(bb1 != bb2)
+
+
+class X86CfgRecoveryTests(unittest.TestCase):
+
+    def setUp(self):
+        self._arch_mode = ARCH_X86_MODE_32
+        self._arch_info = X86ArchitectureInformation(architecture_mode=self._arch_mode)
+        self._disassembler = X86Disassembler()
+        self._translator = X86Translator()
+
+    def test_sample_1(self):
+        binary = BinaryFile(get_full_path("/data/bin/x86_sample_1"))
+        strategy = RecursiveDescent(self._disassembler, binary.text_section, self._translator, self._arch_info)
+        recoverer = CFGRecoverer(strategy)
+
+        bbs, call_targets = recoverer.build(0x0804840b, 0x08048438)
+
+        self.assertEquals(len(bbs), 1)
+
+        cfg = ControlFlowGraph(bbs, name="main")
+
+        self.assertEquals(cfg.start_address, 0x0804840b)
+        self.assertEquals(cfg.end_address, 0x08048438)
+        self.assertEquals(len(cfg.basic_blocks), 1)
+
+    def test_sample_2(self):
+        binary = BinaryFile(get_full_path("/data/bin/x86_sample_2"))
+        strategy = RecursiveDescent(self._disassembler, binary.text_section, self._translator, self._arch_info)
+        recoverer = CFGRecoverer(strategy)
+
+        # Recover "main" function.
+        bbs, call_targets = recoverer.build(0x0804846d, 0x080484a3)
+
+        self.assertEquals(len(bbs), 4)
+
+        cfg = ControlFlowGraph(bbs, name="main")
+
+        self.assertEquals(cfg.start_address, 0x0804846d)
+        self.assertEquals(cfg.end_address, 0x080484a3)
+        self.assertEquals(len(cfg.basic_blocks), 4)
+
+        bb_entry = cfg.find_basic_block(0x0804846d)
+        self.assertEquals(len(bb_entry.branches), 2)
+        self.assertEquals(bb_entry.taken_branch, 0x08048491)
+        self.assertEquals(bb_entry.not_taken_branch, 0x0804848a)
+
+        bb_taken = cfg.find_basic_block(0x08048491)
+        self.assertEquals(len(bb_taken.branches), 1)
+        self.assertEquals(bb_taken.taken_branch, None)
+        self.assertEquals(bb_taken.not_taken_branch, None)
+        self.assertEquals(bb_taken.direct_branch, 0x08048496)
+
+        bb_not_taken = cfg.find_basic_block(0x0804848a)
+        self.assertEquals(len(bb_not_taken.branches), 1)
+        self.assertEquals(bb_not_taken.taken_branch, None)
+        self.assertEquals(bb_not_taken.not_taken_branch, None)
+        self.assertEquals(bb_not_taken.direct_branch, 0x08048496)
+
+        # Recover "func_1" function.
+        bbs, call_targets = recoverer.build(0x0804843b, 0x8048453)
+
+        self.assertEquals(len(bbs), 1)
+
+        cfg = ControlFlowGraph(bbs, name="main")
+
+        self.assertEquals(cfg.start_address, 0x0804843b)
+        self.assertEquals(cfg.end_address, 0x8048453)
+        self.assertEquals(len(cfg.basic_blocks), 1)
+
+        # Recover "func_2" function.
+        bbs, call_targets = recoverer.build(0x08048454, 0x0804846c)
+
+        self.assertEquals(len(bbs), 1)
+
+        cfg = ControlFlowGraph(bbs, name="main")
+
+        self.assertEquals(cfg.start_address, 0x08048454)
+        self.assertEquals(cfg.end_address, 0x0804846c)
+        self.assertEquals(len(cfg.basic_blocks), 1)
 
 
 def main():
