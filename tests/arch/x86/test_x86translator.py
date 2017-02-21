9924fabbd5218250e6818177578dea3e0e76fb27
Christian Heitman <cnheitman@fundacionsadosky.org.ar>
2017-02-21 16:41:59 -0300

    Add support for shrd instruction

--- tests/arch/x86/test_x86translator.py
+++ tests/arch/x86/test_x86translator.py
@@ -2265,6 +2265,22 @@ class X86TranslationTests(unittest.TestCase):
 
         self.assertTrue(cmp_result, self.__print_contexts(ctx_init, x86_ctx_out, reil_ctx_out))
 
+    def test_shrd_1(self):
+        asm = ["shrd eax, ebx, 3"]
+
+        ctx_init = self.__init_context()
+
+        x86_ctx_out, reil_ctx_out = self.__run_code(asm, 0xdeadbeef, ctx_init)
+
+        # TODO Test flags.
+
+        cmp_result = self.__compare_contexts(ctx_init, x86_ctx_out, reil_ctx_out)
+
+        if not cmp_result:
+            self.__save_failing_context(ctx_init)
+
+        self.assertTrue(cmp_result, self.__print_contexts(ctx_init, x86_ctx_out, reil_ctx_out))
+
     def test_shr_1(self):
         asm = ["shr eax, 3"]
 
