b7ef59017b05e78eb3db098cba82115833f68b7e
Christian Heitman <cnheitman@fundacionsadosky.org.ar>
2017-10-02 13:59:12 -0300

    Remove unused import

--- barf/core/smt/smtlibv2.py
+++ barf/core/smt/smtlibv2.py
@@ -25,10 +25,6 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-import logging
-
-logger = logging.getLogger("smtlibv2")
-
 
 class Symbol(object):
 
