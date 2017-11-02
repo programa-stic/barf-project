37c1c6c9a56c9910b43e39461e38a7aea08ce8c4
Christian Heitman <cnheitman@fundacionsadosky.org.ar>
2017-11-02 10:08:44 -0300

    Refactor translate to reil scripts

--- examples/misc/asm_to_reil.py
+++ examples/misc/asm_to_reil.py
@@ -18,3 +18,19 @@ if __name__ == "__main__":
 
         for reil_instr in reil_instrs:
             print("{0:14}{1}".format("", reil_instr))
+
+    #
+    # Open file
+    #
+    barf = BARF("../../samples/bin/branch4.arm")
+
+    #
+    # Translate to REIL
+    #
+    print("[+] Translating x86 to REIL...")
+
+    for addr, asm_instr, reil_instrs in barf.translate(start=0x000083c8, end=0x00008404):
+        print("0x{0:08x} : {1}".format(addr, asm_instr))
+
+        for reil_instr in reil_instrs:
+            print("{0:14}{1}".format("", reil_instr))
