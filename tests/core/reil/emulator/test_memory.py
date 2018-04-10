40d38994e882e2788bf0db2330399135aac4b107
Christian Heitman <cnheitman@fundacionsadosky.org.ar>
2018-04-10 17:00:09 -0300

    Refactor reil emulator test package

---
+++ tests/core/reil/emulator/test_memory.py
@@ -0,0 +1,108 @@
+# Copyright (c) 2014, Fundacion Dr. Manuel Sadosky
+# All rights reserved.
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+
+# 1. Redistributions of source code must retain the above copyright notice, this
+# list of conditions and the following disclaimer.
+
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import unittest
+
+from barf.core.reil.emulator import ReilMemory
+from barf.core.reil.emulator import ReilMemoryEx
+
+
+class ReilMemoryTests(unittest.TestCase):
+
+    def test_write_read_byte_1(self):
+        address_size = 32
+        memory = ReilMemory(address_size)
+
+        addr = 0x00001000
+        write_val = 0xdeadbeef
+
+        memory.write(addr, 32 / 8, write_val)
+        read_val = memory.read(addr, 32 / 8)
+
+        self.assertEqual(write_val, read_val)
+
+    def test_write_read_byte_2(self):
+        address_size = 32
+        memory = ReilMemory(address_size)
+
+        addr = 0x00001000
+        write_val = 0xdeadbeef
+
+        memory.write(addr, 32 / 8, write_val)
+        read_val = memory.read(addr, 32 / 8)
+
+        self.assertEqual(write_val, read_val)
+
+        addr = 0x00001001
+        write_val = 0x1234
+
+        memory.write(addr, 16 / 8, write_val)
+        read_val = memory.read(addr, 16 / 8)
+
+        self.assertEqual(write_val, read_val)
+
+    def test_write_read_byte_3(self):
+        address_size = 32
+        memory = ReilMemory(address_size)
+
+        addr = 0x00001000
+        write_val = 0xdeadbeefcafecafe
+
+        memory.write(addr, 64 / 8, write_val)
+        read_val = memory.read(addr, 64 / 8)
+
+        self.assertEqual(write_val, read_val)
+
+    def test_write_read_byte_4(self):
+        address_size = 32
+        memory = ReilMemoryEx(address_size)
+
+        addr0 = 0x00001000
+        write_val = 0xdeadbeef
+
+        memory.write(addr0, 32 / 8, write_val)
+        read_val = memory.read(addr0, 32 / 8)
+
+        self.assertEqual(write_val, read_val)
+
+        addr1 = 0x00004000
+        write_val = 0xdeadbeef
+
+        memory.write(addr1, 32 / 8, write_val)
+        read_val = memory.read(addr1, 32 / 8)
+
+        self.assertEqual(write_val, read_val)
+
+        addrs = memory.read_inverse(0xdeadbeef, 32 / 8)
+
+        self.assertEqual(addr0, addrs[0])
+        self.assertEqual(addr1, addrs[1])
+
+
+def main():
+    unittest.main()
+
+
+if __name__ == '__main__':
+    main()
---
+++ tests/core/reil/emulator/test_tainter.py
@@ -0,0 +1,141 @@
+# Copyright (c) 2014, Fundacion Dr. Manuel Sadosky
+# All rights reserved.
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+
+# 1. Redistributions of source code must retain the above copyright notice, this
+# list of conditions and the following disclaimer.
+
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import unittest
+
+from barf.arch import ARCH_X86_MODE_32
+from barf.arch.x86 import X86ArchitectureInformation
+from barf.arch.x86.parser import X86Parser
+from barf.arch.x86.translator import X86Translator
+from barf.core.reil.emulator import ReilEmulator
+
+
+class ReilEmulatorTaintTests(unittest.TestCase):
+
+    def setUp(self):
+        self._arch_info = X86ArchitectureInformation(ARCH_X86_MODE_32)
+
+        self._emulator = ReilEmulator(self._arch_info)
+
+        self._asm_parser = X86Parser()
+        self._translator = X86Translator()
+
+    def test_arithmetic(self):
+        asm_instrs  = self._asm_parser.parse("add eax, ebx")
+
+        self.__set_address(0xdeadbeef, [asm_instrs])
+
+        reil_instrs = self._translator.translate(asm_instrs)
+
+        regs_initial = {
+            "eax" : 0x1,
+            "ebx" : 0x2,
+        }
+
+        self._emulator.set_register_taint("ebx", True)
+
+        regs_final, _ = self._emulator.execute_lite(
+            reil_instrs,
+            context=regs_initial
+        )
+
+        self.assertEqual(self._emulator.get_register_taint("eax"), True)
+
+    def test_store_mem_1(self):
+        asm_instrs  = self._asm_parser.parse("mov [eax], ebx")
+
+        self.__set_address(0xdeadbeef, [asm_instrs])
+
+        reil_instrs = self._translator.translate(asm_instrs)
+
+        regs_initial = {
+            "eax" : 0xcafecafe,
+            "ebx" : 0x2,
+        }
+
+        self._emulator.set_register_taint("ebx", True)
+
+        regs_final, _ = self._emulator.execute_lite(
+            reil_instrs,
+            context=regs_initial
+        )
+
+        self.assertEqual(self._emulator.get_memory_taint(regs_initial['eax'], 4), True)
+
+    def test_store_mem_2(self):
+        asm_instrs  = self._asm_parser.parse("mov [eax], ebx")
+
+        self.__set_address(0xdeadbeef, [asm_instrs])
+
+        reil_instrs = self._translator.translate(asm_instrs)
+
+        regs_initial = {
+            "eax" : 0xcafecafe,
+            "ebx" : 0x2,
+        }
+
+        self._emulator.set_register_taint("eax", True)
+
+        regs_final, _ = self._emulator.execute_lite(
+            reil_instrs,
+            context=regs_initial
+        )
+
+        self.assertEqual(self._emulator.get_memory_taint(regs_initial['eax'], 4), False)
+
+    def test_load_mem_1(self):
+        asm_instrs  = self._asm_parser.parse("mov eax, [ebx]")
+
+        self.__set_address(0xdeadbeef, [asm_instrs])
+
+        reil_instrs = self._translator.translate(asm_instrs)
+
+        regs_initial = {
+            "eax" : 0x2,
+            "ebx" : 0xcafecafe,
+        }
+
+        self._emulator.set_memory_taint(regs_initial["ebx"], 4, True)
+
+        regs_final, _ = self._emulator.execute_lite(
+            reil_instrs,
+            context=regs_initial
+        )
+
+        self.assertEqual(self._emulator.get_register_taint("eax"), True)
+
+    def __set_address(self, address, asm_instrs):
+        addr = address
+
+        for asm_instr in asm_instrs:
+            asm_instr.address = addr
+            addr += 1
+
+
+def main():
+    unittest.main()
+
+
+if __name__ == '__main__':
+    main()
