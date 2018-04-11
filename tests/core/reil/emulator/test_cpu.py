f1ef4d8f3c4ed4e4ce97028a2c1b029fce0b74ba
Christian Heitman <cnheitman@fundacionsadosky.org.ar>
2018-04-11 13:33:38 -0300

    Add ReilCpu tests

---
+++ tests/core/reil/emulator/test_cpu.py
@@ -0,0 +1,390 @@
+import unittest
+
+from barf.arch import ARCH_X86_MODE_32
+from barf.arch.x86 import X86ArchitectureInformation
+from barf.core.reil.emulator import ReilCpu
+from barf.core.reil.emulator import ReilMemoryEx
+from barf.core.reil.parser import ReilParser
+
+
+class ReilCpuTests(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    # Arithmetic Instructions
+    def test_add(self):
+        arch = X86ArchitectureInformation(architecture_mode=ARCH_X86_MODE_32)
+
+        mem = ReilMemoryEx(arch.address_size)
+        cpu = ReilCpu(arch, mem)
+
+        parser = ReilParser()
+
+        reil_instr = parser.parse(["add [DWORD t0, DWORD t1, DWORD t2]"])[0]
+        reil_instr.address = 0xcafecafe
+
+        t0 = 0x12345678
+        t1 = 0x1234
+
+        cpu.registers['t0'] = t0
+        cpu.registers['t1'] = t1
+
+        cpu.execute(reil_instr)
+
+        self.assertEquals(t0 + t1, cpu.registers['t2'])
+
+    def test_sub(self):
+        arch = X86ArchitectureInformation(architecture_mode=ARCH_X86_MODE_32)
+
+        mem = ReilMemoryEx(arch.address_size)
+        cpu = ReilCpu(arch, mem)
+
+        parser = ReilParser()
+
+        reil_instr = parser.parse(["sub [DWORD t0, DWORD t1, DWORD t2]"])[0]
+        reil_instr.address = 0xcafecafe
+
+        t0 = 0x12345678
+        t1 = 0x1234
+
+        cpu.registers['t0'] = t0
+        cpu.registers['t1'] = t1
+
+        cpu.execute(reil_instr)
+
+        self.assertEquals(t0 - t1, cpu.registers['t2'])
+
+    def test_mul(self):
+        arch = X86ArchitectureInformation(architecture_mode=ARCH_X86_MODE_32)
+
+        mem = ReilMemoryEx(arch.address_size)
+        cpu = ReilCpu(arch, mem)
+
+        parser = ReilParser()
+
+        reil_instr = parser.parse(["mul [DWORD t0, DWORD t1, DWORD t2]"])[0]
+        reil_instr.address = 0xcafecafe
+
+        t0 = 0x1234
+        t1 = 0x1234
+
+        cpu.registers['t0'] = t0
+        cpu.registers['t1'] = t1
+
+        cpu.execute(reil_instr)
+
+        self.assertEquals(t0 * t1, cpu.registers['t2'])
+
+    def test_div(self):
+        arch = X86ArchitectureInformation(architecture_mode=ARCH_X86_MODE_32)
+
+        mem = ReilMemoryEx(arch.address_size)
+        cpu = ReilCpu(arch, mem)
+
+        parser = ReilParser()
+
+        reil_instr = parser.parse(["div [DWORD t0, DWORD t1, DWORD t2]"])[0]
+        reil_instr.address = 0xcafecafe
+
+        t0 = 0x12345678
+        t1 = 0x1234
+
+        cpu.registers['t0'] = t0
+        cpu.registers['t1'] = t1
+
+        cpu.execute(reil_instr)
+
+        self.assertEquals(t0 / t1, cpu.registers['t2'])
+
+    def test_mod(self):
+        arch = X86ArchitectureInformation(architecture_mode=ARCH_X86_MODE_32)
+
+        mem = ReilMemoryEx(arch.address_size)
+        cpu = ReilCpu(arch, mem)
+
+        parser = ReilParser()
+
+        reil_instr = parser.parse(["mod [DWORD t0, DWORD t1, DWORD t2]"])[0]
+        reil_instr.address = 0xcafecafe
+
+        t0 = 0x12345678
+        t1 = 0x1234
+
+        cpu.registers['t0'] = t0
+        cpu.registers['t1'] = t1
+
+        cpu.execute(reil_instr)
+
+        self.assertEquals(t0 % t1, cpu.registers['t2'])
+
+    def test_bsh_left(self):
+        arch = X86ArchitectureInformation(architecture_mode=ARCH_X86_MODE_32)
+
+        mem = ReilMemoryEx(arch.address_size)
+        cpu = ReilCpu(arch, mem)
+
+        parser = ReilParser()
+
+        reil_instr = parser.parse(["bsh [DWORD t0, DWORD t1, DWORD t2]"])[0]
+        reil_instr.address = 0xcafecafe
+
+        t0 = 0x12345678
+        t1 = 0x8
+
+        cpu.registers['t0'] = t0
+        cpu.registers['t1'] = t1
+
+        cpu.execute(reil_instr)
+
+        self.assertEquals((t0 << t1) & 2**32-1, cpu.registers['t2'])
+
+    def test_bsh_rigt(self):
+        arch = X86ArchitectureInformation(architecture_mode=ARCH_X86_MODE_32)
+
+        mem = ReilMemoryEx(arch.address_size)
+        cpu = ReilCpu(arch, mem)
+
+        parser = ReilParser()
+
+        reil_instr = parser.parse(["bsh [DWORD t0, DWORD t1, DWORD t2]"])[0]
+        reil_instr.address = 0xcafecafe
+
+        t0 = 0x12345678
+        t1 = 0x8
+
+        cpu.registers['t0'] = t0
+        cpu.registers['t1'] = -t1
+
+        cpu.execute(reil_instr)
+
+        self.assertEquals(t0 >> t1, cpu.registers['t2'])
+
+    # Bitwise Instructions
+    def test_and(self):
+        arch = X86ArchitectureInformation(architecture_mode=ARCH_X86_MODE_32)
+
+        mem = ReilMemoryEx(arch.address_size)
+        cpu = ReilCpu(arch, mem)
+
+        parser = ReilParser()
+
+        reil_instr = parser.parse(["and [DWORD t0, DWORD t1, DWORD t2]"])[0]
+        reil_instr.address = 0xcafecafe
+
+        t0 = 0x12345678
+        t1 = 0x1234
+
+        cpu.registers['t0'] = t0
+        cpu.registers['t1'] = t1
+
+        cpu.execute(reil_instr)
+
+        self.assertEquals(t0 & t1, cpu.registers['t2'])
+
+    def test_or(self):
+        arch = X86ArchitectureInformation(architecture_mode=ARCH_X86_MODE_32)
+
+        mem = ReilMemoryEx(arch.address_size)
+        cpu = ReilCpu(arch, mem)
+
+        parser = ReilParser()
+
+        reil_instr = parser.parse(["or [DWORD t0, DWORD t1, DWORD t2]"])[0]
+        reil_instr.address = 0xcafecafe
+
+        t0 = 0x12345678
+        t1 = 0x1234
+
+        cpu.registers['t0'] = t0
+        cpu.registers['t1'] = t1
+
+        cpu.execute(reil_instr)
+
+        self.assertEquals(t0 | t1, cpu.registers['t2'])
+
+    def test_xor(self):
+        arch = X86ArchitectureInformation(architecture_mode=ARCH_X86_MODE_32)
+
+        mem = ReilMemoryEx(arch.address_size)
+        cpu = ReilCpu(arch, mem)
+
+        parser = ReilParser()
+
+        reil_instr = parser.parse(["xor [DWORD t0, DWORD t1, DWORD t2]"])[0]
+        reil_instr.address = 0xcafecafe
+
+        t0 = 0x12345678
+        t1 = 0x1234
+
+        cpu.registers['t0'] = t0
+        cpu.registers['t1'] = t1
+
+        cpu.execute(reil_instr)
+
+        self.assertEquals(t0 ^ t1, cpu.registers['t2'])
+
+    # Data Transfer Instructions
+    def test_ldm(self):
+        arch = X86ArchitectureInformation(architecture_mode=ARCH_X86_MODE_32)
+
+        mem = ReilMemoryEx(arch.address_size)
+        cpu = ReilCpu(arch, mem)
+
+        parser = ReilParser()
+
+        reil_instr = parser.parse(["ldm [DWORD t0, EMPTY, DWORD t1]"])[0]
+        reil_instr.address = 0xcafecafe
+
+        t0 = 0x12345678
+        t1 = 0x1234
+
+        cpu.registers['t0'] = t0
+        cpu.memory.write(t0, 4, t1)
+
+        cpu.execute(reil_instr)
+
+        self.assertEquals(t1, cpu.registers['t1'])
+
+    def test_stm(self):
+        arch = X86ArchitectureInformation(architecture_mode=ARCH_X86_MODE_32)
+
+        mem = ReilMemoryEx(arch.address_size)
+        cpu = ReilCpu(arch, mem)
+
+        parser = ReilParser()
+
+        reil_instr = parser.parse(["stm [DWORD t0, EMPTY, DWORD t1]"])[0]
+        reil_instr.address = 0xcafecafe
+
+        t0 = 0x12345678
+        t1 = 0x1234
+
+        cpu.registers['t0'] = t0
+        cpu.registers['t1'] = t1
+
+        cpu.execute(reil_instr)
+
+        self.assertEquals(t0, cpu.memory.read(t1, 4))
+
+    def test_str(self):
+        arch = X86ArchitectureInformation(architecture_mode=ARCH_X86_MODE_32)
+
+        mem = ReilMemoryEx(arch.address_size)
+        cpu = ReilCpu(arch, mem)
+
+        parser = ReilParser()
+
+        reil_instr = parser.parse(["str [DWORD t0, EMPTY, DWORD t1]"])[0]
+        reil_instr.address = 0xcafecafe
+
+        t0 = 0x12345678
+
+        cpu.registers['t0'] = t0
+
+        cpu.execute(reil_instr)
+
+        self.assertEquals(t0, cpu.registers['t1'])
+
+    # Conditional Instructions
+    def test_bisz(self):
+        arch = X86ArchitectureInformation(architecture_mode=ARCH_X86_MODE_32)
+
+        mem = ReilMemoryEx(arch.address_size)
+        cpu = ReilCpu(arch, mem)
+
+        parser = ReilParser()
+
+        reil_instr = parser.parse(["BISZ [DWORD t0, EMPTY, BIT t1]"])[0]
+        reil_instr.address = 0xcafecafe
+
+        t0 = 0x12345678
+
+        cpu.registers['t0'] = t0
+
+        cpu.execute(reil_instr)
+
+        self.assertEquals(1 if t0 == 0 else 0, cpu.registers['t1'])
+
+    def test_jcc(self):
+        arch = X86ArchitectureInformation(architecture_mode=ARCH_X86_MODE_32)
+
+        mem = ReilMemoryEx(arch.address_size)
+        cpu = ReilCpu(arch, mem)
+
+        parser = ReilParser()
+
+        reil_instr = parser.parse(["jcc [BIT t0, EMPTY, POINTER t1]"])[0]
+        reil_instr.address = 0xcafecafe
+
+        t0 = 0x1
+        t1 = 0x1234567800
+
+        cpu.registers['t0'] = t0
+        cpu.registers['t1'] = t1
+
+        next_ip = cpu.execute(reil_instr)
+
+        self.assertEquals(t1, next_ip)
+
+    # Extensions
+    def test_sext(self):
+        arch = X86ArchitectureInformation(architecture_mode=ARCH_X86_MODE_32)
+
+        mem = ReilMemoryEx(arch.address_size)
+        cpu = ReilCpu(arch, mem)
+
+        parser = ReilParser()
+
+        reil_instr = parser.parse(["sext [DWORD t0, EMPTY, QWORD t1]"])[0]
+        reil_instr.address = 0xcafecafe
+
+        t0 = 0x12345678
+
+        cpu.registers['t0'] = -t0 & 2**32-1
+
+        cpu.execute(reil_instr)
+
+        self.assertEquals(-t0 & 2**64-1, cpu.registers['t1'])
+
+    def test_sdiv(self):
+        arch = X86ArchitectureInformation(architecture_mode=ARCH_X86_MODE_32)
+
+        mem = ReilMemoryEx(arch.address_size)
+        cpu = ReilCpu(arch, mem)
+
+        parser = ReilParser()
+
+        reil_instr = parser.parse(["sdiv [DWORD t0, DWORD t1, DWORD t2]"])[0]
+        reil_instr.address = 0xcafecafe
+
+        t0 = -0x12345678
+        t1 = -0x1234
+
+        cpu.registers['t0'] = t0
+        cpu.registers['t1'] = t1
+
+        cpu.execute(reil_instr)
+
+        self.assertEquals(t0 / t1, cpu.registers['t2'])
+
+    def test_smod(self):
+        arch = X86ArchitectureInformation(architecture_mode=ARCH_X86_MODE_32)
+
+        mem = ReilMemoryEx(arch.address_size)
+        cpu = ReilCpu(arch, mem)
+
+        parser = ReilParser()
+
+        reil_instr = parser.parse(["smod [DWORD t0, DWORD t1, DWORD t2]"])[0]
+        reil_instr.address = 0xcafecafe
+
+        t0 = -0x12345678
+        t1 = -0x1234
+
+        cpu.registers['t0'] = t0
+        cpu.registers['t1'] = t1
+
+        cpu.execute(reil_instr)
+
+        self.assertEquals((t0 % t1) & 2**32-1, cpu.registers['t2'])
